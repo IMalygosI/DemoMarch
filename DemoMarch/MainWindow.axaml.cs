85d393154020c4a49c4436f13ed2fc0f9f8d4fe4
IMalygosI <dmitriy.udaltcov@gmail.com>
2025-03-11 10:44:27 +0300

    First commit

---
+++ DemoMarch/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DemoMarch
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
